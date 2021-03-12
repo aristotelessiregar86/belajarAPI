1bc9efaece38078dd2b775f10d429e5f5817d641
aristotelessiregar86 <aristotelessiregar89@gmail.com>
2021-03-12 22:58:21 +0700

    Setting Routes Diserver JS

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 // parse aplication/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// Panggil router
+const routes = require('./router');
+routes(app);
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
